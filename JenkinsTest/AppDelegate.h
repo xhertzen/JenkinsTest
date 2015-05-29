88dda2f3116f6bae7c2dedabdee75b7058a666d7
Xenia H <xenia.hertzenberg@fluke.com>
2015-05-29 15:41:59 -0700

    initial commit

---
+++ JenkinsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JenkinsTest
+//
+//  Created by Xenia Hertzenberg on 5/29/15.
+//  Copyright (c) 2015 счастливая случайность. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JenkinsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JenkinsTest
+//
+//  Created by Xenia Hertzenberg on 5/29/15.
+//  Copyright (c) 2015 счастливая случайность. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
